d8ef0867bd8fa8ae12f3980737647ce48b520e95
Karen Stewart <kastewart2002@googlemail.com>
2021-10-12 23:04:50 +0100

    second MVP paint test passed

--- paint.js
+++ paint.js
@@ -2,6 +2,15 @@ const Paint = function(numOfLitres){
     this.numOfLitres = numOfLitres;
 };
 
+Paint.prototype.isTinEmpty = function(){
+    let result;
+    if (this.numOfLitres === 0){
+        result = true;
+    }else{
+        result = false;
+    }
+    return result;
+};
 
 
 
