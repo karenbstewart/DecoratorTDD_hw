964da71e9966cd11b1354fb077c8d292a236d3b2
Karen Stewart <kastewart2002@googlemail.com>
2021-10-12 23:09:30 +0100

    third test for paint MVP written

--- specs/paint_specs.js
+++ specs/paint_specs.js
@@ -9,15 +9,17 @@ describe('Paint', function(){
         paint = new Paint(5)
     });
 
-    it('have a number of litres of paint', function(){
+    it('should have a number of litres of paint', function(){
         const actual = paint.numOfLitres;
         assert.strictEqual(actual, 5)
     });
-    it('be able to check if it is empty', function(){
+    it('should be able to check if it is empty', function(){
         const actual = paint.isTinEmpty(); 
         assert.strictEqual(actual, false);
     });
-
+    it('should be able to empty itself of paint', function(){
+        const actual = paint.emptyTin(); 
+        assert.strictEqual(actual, 0);
 
 
 
