cf33c3692a3528c093f5f841d2318c6148b77dc5
Karen Stewart <kastewart2002@googlemail.com>
2021-10-12 23:54:58 +0100

    test written for be able to add a tin of paint to paint stock

--- specs/decorator_specs.js
+++ specs/decorator_specs.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { isFunction } = require('util');
 const Decorator = require('../decorator.js');
+const Paint = require('../paint.js');
 
 describe('Decorator', function(){
     let decorator;
@@ -10,6 +11,12 @@ describe('Decorator', function(){
     });
     it('should have an empty paint stock', function(){
         const actual = decorator.paintStock;
+        assert.deepStrictEqual(actual, []);
+    });
+    it('be able to add a tin of paint to the paint stock', function(){
+        const paint = new Paint(7);
+        decorator.addPaintToStock('Paint')
+        const actual = decorator.paintStock.length;
         assert.deepStrictEqual(actual, []);
     });
 
